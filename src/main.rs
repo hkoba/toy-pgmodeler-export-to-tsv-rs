080d5871ea45df8e6bda6a4158f7813804644cb9
Kobayasi, Hiroaki <buribullet@gmail.com>
2019-11-29 17:47:40 +0900

    Successfully parsed (partial) PostgreSQL DDL (exported from pgModeler)

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
-
+use std::fs::File;
+//use std::io::Read;
+use std::io::prelude::*;
 
 use sqlparser::dialect::GenericDialect;
 use sqlparser::parser::Parser;
 
-fn main() {
-
-    let sql = "SELECT a, b, 123, myfunc(b) \
-               FROM table_1 \
-               WHERE a > b AND b < 100 \
-               ORDER BY a DESC, b";
+fn main() -> std::io::Result<()> {
 
     let dialect = GenericDialect {}; // or AnsiDialect, or your own dialect ...
-
-    let ast = Parser::parse_sql(&dialect, sql.to_string()).unwrap();
-
-    println!("AST: {:?}", ast);
+    
+    let mut fit = std::env::args();
+    fit.next();
+    
+    while let Some(fname) = fit.next() {
+        println!("file = {}", fname);
+        let mut fh = File::open(fname)?;
+        let mut sql = String::new();
+        fh.read_to_string(&mut sql)?;
+        
+        for ddl in sql.split("\n-- ddl-end --\n") {
+            match Parser::parse_sql(&dialect, ddl.to_string()) {
+                Ok(ast) => {
+                    println!("====\n{}\n====", ddl);
+                    println!("");
+                    println!("AST: {:?}", ast)
+                },
+                Err(_) => (),
+            }
+        }
+    }
+    
+    Ok(())
 }
