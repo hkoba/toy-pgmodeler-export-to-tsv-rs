f3a0a12751a8b8a14bb8e719cb88883bcc9655a5
Kobayasi, Hiroaki <buribullet@gmail.com>
2019-11-29 17:23:55 +0900

    From README.md

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+
+
+use sqlparser::dialect::GenericDialect;
+use sqlparser::parser::Parser;
+
 fn main() {
-    println!("Hello, world!");
+
+    let sql = "SELECT a, b, 123, myfunc(b) \
+               FROM table_1 \
+               WHERE a > b AND b < 100 \
+               ORDER BY a DESC, b";
+
+    let dialect = GenericDialect {}; // or AnsiDialect, or your own dialect ...
+
+    let ast = Parser::parse_sql(&dialect, sql.to_string()).unwrap();
+
+    println!("AST: {:?}", ast);
 }
